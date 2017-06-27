c831fb14959dca3d01b0f5815d3c50f7dccee516
Mike Molenda <molenda.mike@gmail.com>
2017-06-27 16:20:35 -0500

    Create collection users

--- sample-data.js
+++ sample-data.js
@@ -1,12 +1,14 @@
 // Connect to DB instance
 var conn = new Mongo("localhost:27017");
-var db = conn.getDB("node-auth");
+var db = conn.getDB("nodeauth");
 
 // Drop existing collections, if any
-if (db.user.drop()) {
-    print("- DROPPED COLLECTION user");
+if (db.users.drop()) {
+    console.log("[INFO] Dropped collection users");
 } else {
-    print("! DID NOT DROP COLLECTION user");
+    console.log("[WARN] Drop collection users failed or users does not exist");
 }
 
 // Create collections
+db.users.insert({name: 'Guy Incognito', email: 'test@test.com', username: 'test', password: 'abc123'});
+

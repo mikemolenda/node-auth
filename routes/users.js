6a22a8e1bc1ecfa7a504b81218e08112c7df332b
Mike Molenda <molenda.mike@gmail.com>
2017-06-28 10:45:31 -0500

    Create new user registration form

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
+/* GET users/register listing. */
+router.get('/register', function(req, res, next) {
+    res.render('register', {title: 'Register'});
+});
+
 // Enable access from external modules
 module.exports = router;

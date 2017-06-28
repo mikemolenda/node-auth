f5bd4372c3ffb9d5709b792b80aab4d08164719b
Mike Molenda <molenda.mike@gmail.com>
2017-06-28 10:57:45 -0500

    Create login form view and routing

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
-/* GET users/register listing. */
+/* GET users/register */
 router.get('/register', function(req, res, next) {
     res.render('register', {title: 'Register'});
+});
+
+/* GET users/login */
+router.get('/login', function(req, res, next) {
+    res.render('login', {title: 'Login'});
 });
 
 // Enable access from external modules

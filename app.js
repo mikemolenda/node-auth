fffe589c91f57603101d337e00a0ead279825279
Mike Molenda <molenda.mike@gmail.com>
2017-06-28 08:10:56 -0500

    Replace 'var's with 'const's and 'let's

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
-var localStrategy = require('passport-local').Strategy;
-var multer = require('multer');
-var flash = require('connect-flash');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const multer = require('multer');
+const flash = require('connect-flash');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // Establish DB connection
 const db = mongoose.connection;
@@ -37,7 +37,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });

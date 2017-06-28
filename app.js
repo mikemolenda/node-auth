fe2557cf20cda875df63b424e2388ebeed1511e3
Mike Molenda <molenda.mike@gmail.com>
2017-06-28 08:09:57 -0500

    Establish MongoDB connection

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Establish DB connection
+const db = mongoose.connection;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');

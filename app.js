7bfc034f556b970f3a0b5b5b75155e9299614626
Mike Molenda <molenda.mike@gmail.com>
2017-06-28 08:31:49 -0500

    Change view engine specifier to Pug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const db = mongoose.connection;
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // Express bindings
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));

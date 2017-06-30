6653db234e8a0abfd4b6a396863a01589efa97d2
Mike Molenda <molenda.mike@gmail.com>
2017-06-30 13:42:07 -0500

    Implement Passport.js authentication

--- models/user.js
+++ models/user.js
@@ -26,6 +26,25 @@ let UserSchema = mongoose.Schema({
 
 let User = module.exports = mongoose.model('User', UserSchema);
 
+/* Retrieve a User object from the DB by its ID */
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+};
+
+/* Retrieve a User object from the DB by username value */
+module.exports.getUserByUsername = function(username, callback) {
+    let query = {username: username};
+    User.findOne(query, callback);
+};
+
+/* Compare a supplied password with a user password hash from the DB */
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        callback(null, isMatch);
+    });
+};
+
+/* Create a new user with passed information and hashed password */
 module.exports.createUser = function(newUser, callback) {
     // Hash password with BCrypt
     bcrypt.genSalt(10, function (err, salt) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 let User = require('../models/user');
 
@@ -84,6 +86,58 @@ router.post('/register', upload.single('profilepic'), function(req, res, next) {
     }
 
 });
+
+/* POST users/login */
+router.post('/login',
+    passport.authenticate('local', {failureRedirect: '/users/login', failureFlash: 'Incorrect username or password'}),
+    function(req, res) {
+        // If this anonymous function gets called, authentication was successful
+        req.flash('success', 'Successful login');
+        res.redirect('/');
+    }
+);
+
+// Serialize user ID to session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// Deserialize user from session by ID
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// Set up passport to use local DB for authentication
+passport.use(new LocalStrategy(function(username, password, done) {
+
+    User.getUserByUsername(username, function(err, user) {
+        if (err) {
+            throw err;
+        }
+
+        // If username not found, respond with message
+        if (!user) {
+            return done(null, false, {message: 'Username not found'});
+        }
+
+        // If user found, verify password
+        User.comparePassword(password, user.password, function(err, isMatch) {
+            if (err) {
+                return done(err);
+            }
+
+            // If passwords match login, otherwise respond with message
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, {message: 'Incorrect password'});
+            }
+        });
+    });
+
+}));
 
 // Enable access from external modules
 module.exports = router;

7b27e53068d1b3e044e891ae41d4e2a7fe112f65
Mike Molenda <molenda.mike@gmail.com>
2017-06-30 09:10:59 -0500

    Update sample-data.js

--- sample-data.js
+++ sample-data.js
@@ -1,12 +1,12 @@
 // Connect to DB instance
-var conn = new Mongo("localhost:27017");
-var db = conn.getDB("nodeauth");
+var conn = new Mongo('localhost:27017');
+var db = conn.getDB('nodeauth');
 
 // Drop existing collections, if any
 if (db.users.drop()) {
-    console.log("[INFO] Dropped collection users");
+    print('Dropped collection users');
 } else {
-    console.log("[WARN] Drop collection users failed or users does not exist");
+    print('Drop collection users failed or users does not exist');
 }
 
 // Create collections
